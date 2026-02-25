0cfbcc000134835e353e75899184d0eb704474a5
Chip <chip@bleedingretro.com>
2026-02-25 13:29:27 -0500

    Set site URL and base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://dubesinhower.github.io',
+  base: '/dmc-wiki',
   integrations: [
     starlight({
       title: 'DMC Wiki',
